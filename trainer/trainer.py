ba4ff1de69be393e0c366aa1e1c2efec99597b7c
Bhoomi Priya <126129399+Bhoomi139@users.noreply.github.com>
2026-07-09 06:08:42 +0530

    Refactor Trainer class for improved readability

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,17 +1,17 @@
 import numpy as np
 import torch
-import torch.nn.functional as F
-from torch.autograd import Variable
 import time
 import sys
 import os
 
+
 class Trainer():
-    def __init__(self, model, model_type, loss_fn, optimizer, lr_schedule, log_batchs, is_use_cuda, train_data_loader, \
-                valid_data_loader=None, metric=None, start_epoch=0, num_epochs=25, is_debug=False, logger=None, writer=None):
+    def __init__(self, model, model_type, loss_fn, optimizer, lr_schedule, log_batchs, is_use_cuda, train_data_loader,
+                 valid_data_loader=None, metric=None, start_epoch=0, num_epochs=25, is_debug=False, logger=None,
+                 writer=None):
         self.model = model
         self.model_type = model_type
-        self.loss_fn  = loss_fn
+        self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.lr_schedule = lr_schedule
         self.log_batchs = log_batchs
@@ -21,7 +21,7 @@ class Trainer():
         self.start_epoch = start_epoch
         self.num_epochs = num_epochs
         self.is_debug = is_debug
-        
+
         self.cur_epoch = start_epoch
         self.best_mae = sys.float_info.max
         self.logger = logger
@@ -29,116 +29,145 @@ class Trainer():
 
     def fit(self):
         for epoch in range(self.start_epoch, self.num_epochs):
-            self.logger.append('Epoch {}/{}'.format(epoch, self.num_epochs - 1))
-            self.logger.append('-' * 60)
             self.cur_epoch = epoch
-            self.lr_schedule.step()
+
+            print(f"\nEpoch [{epoch + 1}/{self.num_epochs}]")
+            print("-" * 60)
+
             if self.is_debug:
                 self._dump_infos()
-            train_loss, train_mae=self._train()
-            val_loss, val_mae=self._valid()
+
+            train_loss, train_mae = self._train()
+            val_loss, val_mae = self._valid()
+
             self.logger.append(
-                f"Epoch {epoch+1} Summary | "
+                f"Epoch {epoch + 1} Summary | "
                 f"Train Loss: {train_loss:.4f} | "
                 f"Train MAE: {train_mae:.4f} | "
                 f"Val Loss: {val_loss:.4f} | "
                 f"Val MAE: {val_mae:.4f}"
-)
+            )
+
+            print(
+                f"Train Loss: {train_loss:.4f} | "
+                f"Train MAE: {train_mae:.4f}"
+            )
+            print(
+                f"Val Loss: {val_loss:.4f} | "
+                f"Val MAE: {val_mae:.4f}"
+            )
+
             self._save_best_model(val_mae)
-            print()
+
+            self.lr_schedule.step()
 
     def _dump_infos(self):
         self.logger.append('---------------------Current Parameters---------------------')
         self.logger.append('is use GPU: ' + ('True' if self.is_use_cuda else 'False'))
-        self.logger.append('lr: %f' % (self.lr_schedule.get_lr()[0]))
+        self.logger.append('lr: %f' % (self.lr_schedule.get_last_lr()[0]))
         self.logger.append('model_type: %s' % (self.model_type))
         self.logger.append('current epoch: %d' % (self.cur_epoch))
         self.logger.append('best MAE: %f' % (self.best_mae))
         self.logger.append('------------------------------------------------------------')
 
     def _train(self):
-        self.model.train()  # Set model to training mode
-        losses = []
-        maes=[]
+        self.model.train()
 
-        for i, (inputs, labels) in enumerate(self.train_data_loader):              # Notice
+        running_loss = 0.0
+        running_mae = 0.0
+        total_samples = 0
+
+        for inputs, labels in self.train_data_loader:
+
             if self.is_use_cuda:
-                inputs, labels = inputs.cuda(), labels.cuda()
-                labels = labels.squeeze()
-            else:
-                labels = labels.squeeze()
+                inputs = inputs.cuda()
+                labels = labels.cuda()
+
+            labels = labels.squeeze()
 
             self.optimizer.zero_grad()
-            outputs = self.model(inputs)   
-            outputs = outputs.squeeze(1)         # Notice 
+
+            outputs = self.model(inputs)
+            outputs = outputs.squeeze(1)
+
             loss = self.loss_fn[0](outputs, labels)
             mae = torch.mean(torch.abs(outputs - labels))
+
             loss.backward()
             self.optimizer.step()
 
-            losses.append(loss.item())
-            maes.append(mae.item())       # Notice        
+            batch_size = inputs.size(0)
+
+            running_loss += loss.item() * batch_size
+            running_mae += mae.item() * batch_size
+            total_samples += batch_size
+
+        epoch_loss = running_loss / total_samples
+        epoch_mae = running_mae / total_samples
+
         if self.writer:
             self.writer.add_scalar(
                 'train/loss',
-                 batch_mean_loss,
+                epoch_loss,
                 self.cur_epoch
             )
             self.writer.add_scalar(
                 'train/mae',
-                 batch_mean_maes,
+                epoch_mae,
                 self.cur_epoch
             )
-        return batch_mean_loss, batch_mean_maes  
 
+        return epoch_loss, epoch_mae
 
     def _valid(self):
         self.model.eval()
-        losses = []
-        maes=[]
-        acc_rate = 0.
 
-        with torch.no_grad():              # Notice
-            for i, (inputs, labels) in enumerate(self.valid_data_loader):
+        running_loss = 0.0
+        running_mae = 0.0
+        total_samples = 0
+
+        with torch.no_grad():
+
+            for inputs, labels in self.valid_data_loader:
+
                 if self.is_use_cuda:
-                    inputs, labels = inputs.cuda(), labels.cuda()
-                    labels = labels.squeeze()
-                else:
-                    labels = labels.squeeze()
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+
+                labels = labels.squeeze()
 
                 outputs = self.model(inputs)
-                outputs=outputs.squeeze(1)           # Notice 
-                loss = self.loss_fn[0](outputs, labels)
-                mae = torch.mean(torch.abs(outputs-labels))
-                losses.append(loss.item())
-                maes.append(mae.item())
-            
-        local_time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        #self.logger.append(losses)
-        batch_mean_loss = np.mean(losses)
-        batch_mean_maes =np.mean(maes)
-    
-        print_str = '[%s]\tValidation:\t Loss: %.4f\t MAE: %.4f\t' \
-            % (local_time_str, batch_mean_loss, batch_mean_maes)
+                outputs = outputs.squeeze(1)
 
-        self.logger.append(print_str)
+                loss = self.loss_fn[0](outputs, labels)
+                mae = torch.mean(torch.abs(outputs - labels))
+
+                batch_size = inputs.size(0)
+
+                running_loss += loss.item() * batch_size
+                running_mae += mae.item() * batch_size
+                total_samples += batch_size
+
+        epoch_loss = running_loss / total_samples
+        epoch_mae = running_mae / total_samples
+
         if self.writer:
             self.writer.add_scalar(
                 'val/loss',
-                 batch_mean_loss,
+                epoch_loss,
                 self.cur_epoch
             )
             self.writer.add_scalar(
                 'val/mae',
-                 batch_mean_maes,
+                epoch_mae,
                 self.cur_epoch
             )
 
-        return batch_mean_loss, batch_mean_maes 
-
+        return epoch_loss, epoch_mae
 
     def _save_best_model(self, val_mae):
         if val_mae < self.best_mae:
+
             self.best_mae = val_mae
 
             self.logger.append(
@@ -161,6 +190,7 @@ class Trainer():
                 state,
                 save_dir + '/best_model.ckpt'
             )
+
         else:
             self.logger.append(
                 'Validation MAE did not improve. Model not saved.'
